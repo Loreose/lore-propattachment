7c330745753e2cebd668009674519f6dd365394c
Loreose <yyagizozcicekk@gmail.com>
2026-09-19 00:55:08 +0300

    Bu bir testtir

--- config.lua
+++ config.lua
@@ -2,7 +2,6 @@ Config = {}
 
 Config.Debug = true
 
-
 function debug(...)
     if Config.Debug then print("^3[DEBUG]^7", ...) end
 end
--- server/main.lua
+++ server/main.lua
@@ -1,8 +1,7 @@
-
-AddEventHandler("onResourceStart", function (resourceName)
+AddEventHandler("onResourceStart", function(resourceName)
     if (GetCurrentResourceName() ~= resourceName) then return end
 end)
 
-AddEventHandler("onResourceStop", function (resourceName)
+AddEventHandler("onResourceStop", function(resourceName)
     if (GetCurrentResourceName() ~= resourceName) then return end
 end)
